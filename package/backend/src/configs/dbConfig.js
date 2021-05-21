4210280bac5e19ec525ccac76c13bde035f8aa94
Phuong <tuanphuongtran.14@gmail.com>
2021-05-21 16:50:23 +0700

    Change url config for database

--- package/backend/src/configs/dbConfig.js
+++ package/backend/src/configs/dbConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PW}@localhost:27017/${process.env.APP_DB_NAME}?authSource=admin`
+    // url: `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PW}@localhost:27017/${process.env.APP_DB_NAME}?authSource=admin`
+    url: `localhost:27017/${process.env.APP_DB_NAME}`
 }
